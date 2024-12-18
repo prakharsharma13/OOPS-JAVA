2f76a10549d3f0c1793b00831926e13ffaf83820
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-12-18 17:33:11 +0530

    Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,17 @@
+class Shape {
+    public void area() {
+        System.out.println("Display's area");
+    }
+}
+
+class Triangle extends Shape {
+    public void area(int b, int h) {
+        System.out.println(1 / 2 * b * h);
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+    }
+}
